e578a0e20ceb2891fee699890db1ff8c602a9c3c
Sebastian <vetkin01@hotmail.com>
2017-11-06 15:59:34 -0700

    Primer ejemplo JS

---
+++ js/javascript01.js
@@ -0,0 +1,12 @@
+var datos = function()
+		{
+			var nombre;
+			var edad;
+			nombre = prompt("Teclea tu nombre");
+			edad = prompt("Teclea tu edad");
+			document.write("Hola ");
+			document.write(nombre);
+			document.write(" tienes ");
+			document.write(edad)
+			document.write(" años");
+		}
